cc8b9eafefcc55fe919cad5051b0a5da44c69da4
Eng-Maoga <mmichaelowen003@gmail.com>
2025-08-05 23:22:58 +0300

    updated list

--- my_list.py
+++ my_list.py
@@ -14,5 +14,5 @@ sorted_list = sorted(my_list)
 print("Original list:", my_list)
 print("Sorted list:", sorted_list)
 
-index_of_30 =my_list.index(30)
+index_of_30 = my_list.index(30)
 print(f"The index of 30 is: {index_of_30}")
